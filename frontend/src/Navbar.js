b4c6551331c65af0e128174d8a7efa4970f529ba
nishkarshkewlani <nishkarshkewlani@gmail.com>
2026-06-01 22:20:43 +0530

    Add Navbar component with styling

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,18 @@
+export default function Navbar() {
+
+  return (
+
+    <div style={{
+      backgroundColor: "#111827",
+      color: "white",
+      padding: "15px",
+      fontSize: "24px",
+      fontWeight: "bold"
+    }}>
+
+      MetroMind AI
+
+    </div>
+
+  );
+}
